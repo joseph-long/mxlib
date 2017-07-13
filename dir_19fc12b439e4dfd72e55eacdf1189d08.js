ffbf127528a3441357805ae9d856d5d0150b4db5
Jared <jaredmales@gmail.com>
2017-07-12 23:21:30 -0700

    small doc updates

--- dir_19fc12b439e4dfd72e55eacdf1189d08.js
+++ dir_19fc12b439e4dfd72e55eacdf1189d08.js
@@ -1,7 +1,5 @@
 var dir_19fc12b439e4dfd72e55eacdf1189d08 =
 [
-    [ "trueFalseT.hpp", "trueFalseT_8hpp.html", [
-      [ "trueFalseT", "structmx_1_1meta_1_1trueFalseT.html", null ]
-    ] ],
+    [ "trueFalseT.hpp", "trueFalseT_8hpp.html", null ],
     [ "typeTraits.hpp", "typeTraits_8hpp.html", null ]
 ];
--- group__meta.js
+++ group__meta.js
@@ -1,5 +1,6 @@
 var group__meta =
 [
+    [ "trueFalseT", "structmx_1_1meta_1_1trueFalseT.html", null ],
     [ "trueFalseT< true >", "structmx_1_1meta_1_1trueFalseT_3_01true_01_4.html", [
       [ "True", "structmx_1_1meta_1_1trueFalseT_3_01true_01_4.html#a4be3ccac7925c0cfbc7425c01542a7b8", null ],
       [ "value", "structmx_1_1meta_1_1trueFalseT_3_01true_01_4.html#a80897842e47e3277f8296611a0138534", null ]
