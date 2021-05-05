303d0d3c539164e5b6079635d856d391582138fb
Joseph Long <github@joseph-long.com>
2021-05-05 14:53:37 -0700

    .

--- setup/conda_env_setup.bash
+++ setup/conda_env_setup.bash
@@ -2,4 +2,5 @@
 
 function install_common_packages() {
     conda install -y pkg-config cmake cfitsio cppzmq eigen boost mkl mkl-include gsl fftw
+    conda env config vars set MKLROOT=$CONDA_PREFIX
 }
