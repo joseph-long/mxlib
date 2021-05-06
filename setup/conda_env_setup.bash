9aac7d2976a4b219cc2818abc92ba75f7c1a7818
Joseph Long <github@joseph-long.com>
2021-05-05 19:51:39 -0700

    Set pkg config path

--- setup/conda_env_setup.bash
+++ setup/conda_env_setup.bash
@@ -3,5 +3,5 @@
 function install_common_packages() {
     conda install -y pkg-config cmake cfitsio cppzmq eigen boost mkl mkl-include gsl fftw
     conda env config vars set MKLROOT="$CONDA_PREFIX"
-    conda env config vars set PKG_CONFIG_PATH="$CONDA_PREFIX/share/pkgconfig:$CONDA_PREFIX/lib/pkgconfig:$PKG_CONFIG_PATH"
+    conda env config vars set PKG_CONFIG_PATH="$CONDA_PREFIX/share/pkgconfig:$CONDA_PREFIX/lib/pkgconfig:${PKG_CONFIG_PATH:-}"
 }
